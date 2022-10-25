236357f0e756f0fd2120a1a78cf573765471e088
Taras Drozdovskyi <t.drozdovsky@samsung.com>
2022-10-25 16:35:25 +0300

    Fixed CVE-2022-38155, CVE-2022-40762
    Signed-off-by: Taras Drozdovskyi <t.drozdovsky@samsung.com>

--- tee/lib/libutee/tee_user_mem.c
+++ tee/lib/libutee/tee_user_mem.c
@@ -43,9 +43,14 @@
 
 #define CFG_TEE_CORE_USER_MEM_DEBUG 0
 
+extern volatile __HeapBase;
+extern volatile __HeapLimit;
+
 void *tee_user_mem_alloc(size_t len, uint32_t hint)
 {
 	uint8_t *p;
+	void* limit = &__HeapLimit;
+	void* base = &__HeapBase;
 
 	switch (hint) {
 	case TEE_MALLOC_FILL_ZERO:
@@ -55,9 +60,18 @@ void *tee_user_mem_alloc(size_t len, uint32_t hint)
 		EMSG("Invalid alloc hint [%X]", (unsigned int)hint);
 		return NULL;
 	}
-
+	// printf("malloc = base = %x\n", base );
+	// printf("malloc = (limit - base)/2 = %x\n", (limit - base)/2 );
+	if ((limit - base)/2 < len + 8)
+		return NULL;
 	//  p = utee_malloc(len);
 	p = malloc(len);
+	// printf("malloc 1 = base + ((limit - base)/2) = %x\n", base + ((limit - base)/2) );
+	// printf("malloc 2 = p + len = %x\n", p + len );
+	if ( base + ((limit - base)/2) < p + len) {
+		free(p);
+		return NULL;
+	}
 	if (p == NULL)
 		return NULL;
 
@@ -72,13 +86,24 @@ void *tee_user_mem_alloc(size_t len, uint32_t hint)
 
 void *tee_user_mem_realloc(void *buffer, size_t len)
 {
-//  return utee_realloc(buffer, len);
-  return realloc(buffer, len);
+	uint8_t *p;
+	void* limit = &__HeapLimit;
+	void* base = &__HeapBase;
+
+	if ((limit - base)/2 < len + 8)
+		return NULL;
+	p = realloc(buffer, len);
+	if ( base + ((limit - base)/2) < p + len) {
+		free(p);
+		return NULL;
+	}
+//   return utee_realloc(buffer, len);
+	return p;
 }
 
 void tee_user_mem_free(void *buffer)
 {
-  free(buffer);
+	free(buffer);
 //  utee_free(buffer);
 }
 
